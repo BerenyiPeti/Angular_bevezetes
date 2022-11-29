dba5fe370ae8ae83398e5f808be21b4550b80fd2
BerenyiPeti <berenyipeti01@gmail.com>
2022-11-29 10:56:12 +0100

    nav barral lehet navigálni

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentPage = 'recipes'
+
+  constructor() {
+
+  }
+
+  pageSelected(page: string) {
+    this.currentPage = page
+    console.log(page);
+    
+  }
 }
